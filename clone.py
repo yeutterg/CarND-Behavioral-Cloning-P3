55c244e4770461c324c6a5a63f33f57b2389e04d
Greg Yeutter <yeutterg@gmail.com>
2017-04-09 01:10:26 +0000

    add nvidia model

--- clone.py
+++ clone.py
@@ -38,20 +38,35 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 #LeNet model
+#model = Sequential()
+#model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+#model.add(Cropping2D(cropping=((70,25),(0,0))))
+#model.add(Convolution2D(6,5,5,activation="relu"))
+#model.add(MaxPooling2D())
+#model.add(Convolution2D(6,5,5,activation="relu"))
+#model.add(MaxPooling2D())
+#model.add(Flatten())
+#model.add(Dense(120))
+#model.add(Dense(84))
+#model.add(Dense(1))
+
+#NVIDIA model
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5), input_shape=(160,320,3))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,actication="relu"))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
 model.save('model.h5')
 exit()
