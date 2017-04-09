9128296c898e965b3b714fb1889fc9287e302615
Greg Yeutter <yeutterg@gmail.com>
2017-04-09 01:01:16 +0000

    add cropping in LeNet

--- clone.py
+++ clone.py
@@ -13,13 +13,14 @@ lines.pop(0)
 images = []
 measurements = []
 for line in lines:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = './data/IMG/' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('/')[-1]
+		current_path = './data/IMG/' + filename
+		image = cv2.imread(current_path)
+		images.append(image)
+		measurement = float(line[3])
+		measurements.append(measurement)
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -32,15 +33,14 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+#LeNet model
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-#model.add(Flatten(input_shape=(160,320,3)))
-#model.add(Dense(1))
-
-#LeNet
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
@@ -54,4 +54,4 @@ model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
-
+exit()
