68a22e2f6094649d62635e10bcb5dc46d13b6308
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2017-04-20 21:33:03 -0400

    add dropout

--- model.py
+++ model.py
@@ -77,6 +77,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.layers.core import Dropout
 
 #LeNet model
 #model = Sequential()
@@ -98,14 +99,22 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5,
 		output_shape=(ch,row,col)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.4))
 model.add(Dense(50))
+model.add(Dropout(0.3))
 model.add(Dense(10))
+model.add(Dropout(0.2))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
