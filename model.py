0cd58d8dea093cd800f78e0c503661479bd91d8e
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2017-04-22 19:56:40 -0400

    fix errors

--- model.py
+++ model.py
@@ -95,9 +95,9 @@ ch, rw, col = 160, 320, 3  # Trimmed image format
 def lenet_model():
     #LeNet model
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5),
-                     input_shape=(ch, rw, col))
-    model.add(Cropping2D(cropping=((70,25),(0,0))))
+    model.add(Cropping2D(cropping=((70,25),(0,0)),
+                         input_shape=(ch, rw, col)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
     model.add(Convolution2D(6,5,5,activation="relu"))
     model.add(MaxPooling2D())
     model.add(Convolution2D(6,5,5,activation="relu"))
@@ -113,8 +113,9 @@ def lenet_model():
 def nvidia_model(): 
     #NVIDIA model
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5), input_shape=(ch, rw, col))
-    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0)),
+                         input_shape=(ch, rw, col)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
