1c146ed7869cf59832c42e92df784cd3daec65cc
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2017-04-22 18:37:04 -0400

    code cleanup

--- model.py
+++ model.py
@@ -5,28 +5,35 @@ import sklearn
 import numpy as np
 from random import shuffle
 
-lines = []
-with open('./data/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
-
-lines.pop(0)
-
 from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+import matplotlib.pyplot as plt
+
+def import_csv():
+    """
+    Imports the CSV file
+    """
+    lines = []
+    with open('./data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+
+    lines.pop(0) # Remove the header info from the CSV file
+    return lines
 
 def process_img(image):
-    # Crop and scale to 66x200x3
-    img = image[50:140,:,:]
-    img = cv2.resize(img, (200, 66), interpolation=cv2.INTER_AREA)
-
+    """
+    Preprocessing including color space conversion
+    """
     # Convert to YUV color space
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
     return img
 
 
 def generator(samples, batch_size=32):
+    """
+    Generator to reduce memory footprint when training
+    """
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -40,54 +47,29 @@ def generator(samples, batch_size=32):
                     name = './data/IMG/'+batch_sample[i].split('/')[-1]
                     image = cv2.imread(name)
                     angle = float(batch_sample[3])
-                    images.append(image)
+                    img = process_img(image)
+                    images.append(img)
                     angles.append(angle)
 
-            augmented_images, augmented_measurements = [], []
-            for image, measurement in zip(images, angles):
-            	augmented_images.append(image)
-            	augmented_measurements.append(measurement)
-            	augmented_images.append(cv2.flip(image, 1))
-            	augmented_measurements.append(measurement *- 1.0)
-
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# images = []
-# measurements = []
-# for line in lines:
-# 	for i in range(3):
-# 		source_path = line[i]
-# 		filename = source_path.split('/')[-1]
-# 		current_path = './data/IMG/' + filename
-# 		image = cv2.imread(current_path)
-# 		images.append(image)
-# 		measurement = float(line[3])
-# 		measurements.append(measurement)
-
-# augmented_images, augmented_measurements = [], []
-# for image, measurement in zip(images, measurements):
-# 	augmented_images.append(image)
-# 	augmented_measurements.append(measurement)
-# 	augmented_images.append(cv2.flip(image, 1))
-# 	augmented_measurements.append(measurement *- 1.0)
-
-# X_train = np.array(images)
-# y_train = np.array(measurements)
+# Import the data and split into training and validation sets
+lines = import_csv()
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
-
-ch, row, col = 66, 200, 3  # Trimmed image format
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.core import Dropout
+
+ch, row, col = 160, 320, 3  # Trimmed image format
 
 #LeNet model
 #model = Sequential()
@@ -104,15 +86,14 @@ from keras.layers.core import Dropout
 
 #NVIDIA model
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, 
-		input_shape=(ch,row,col), 
-		output_shape=(ch,row,col)))
-# model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Lambda(lambda x: x / 255.0 - 0.5))
+model.add(Cropping2D(cropping=((70, 25), (0, 0)),
+                     input_shape=(ch, row, col)))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -120,10 +101,22 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3, verbose=1)
 
-model.fit_generator(train_generator, samples_per_epoch=len(3*train_samples),					validation_data=validation_generator,
-		nb_val_samples=len(validation_samples), nb_epoch=5)
+fitgen = model.fit_generator(train_generator,
+                             samples_per_epoch=len(3*train_samples),
+                             validation_data=validation_generator,
+                             nb_val_samples=len(validation_samples),
+                             nb_epoch=5)
 
 model.save('model.h5')
+
+# Plot training and validation loss
+plt.plot(fitgen.history['loss'])
+plt.plot(fitgen.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig("training_validation_loss_plot.jpg")
+
 exit()
