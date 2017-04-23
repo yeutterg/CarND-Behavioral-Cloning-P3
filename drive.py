a626325dce6813f74cae9f787e0d511af4f28b18
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2017-04-23 11:28:44 -0400

    remove preprocessing

--- drive.py
+++ drive.py
@@ -69,8 +69,8 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        img = process_img(image_array)
-        steering_angle = float(model.predict(img[None, :, :, :], batch_size=1))
+        # img = process_img(image_array)
+        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
